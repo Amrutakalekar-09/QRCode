3f077bbb4b23b1c58d464fdf026e63434b15f4dd
Amrutakalekar-09 <162716699+Amrutakalekar-09@users.noreply.github.com>
2024-03-19 12:39:59 +0530

    Add files via upload

---
+++ Qrcode.py
@@ -0,0 +1,14 @@
+from warnings import filters
+import qrcode
+import image
+qr = qrcode.QRCode(
+    version = 15,
+    box_size= 10,
+    border= 5
+)
+
+data = "https://github.com/Amruta-kalekar-24"
+qr.add_data(data)
+qr.make(fit = True)
+img = qr.make_image(fill="black",black_color = "white")
+img.save("test.png")
